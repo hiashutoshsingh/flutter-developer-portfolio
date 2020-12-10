89bc33b82322c037c07551c55af4fbff96bff91c
Ashutosh Kumar Singh <ashutosh.singh@innovaccer.com>
2020-12-10 22:54:56 +0530

    social media images added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,6 +8,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String github = 'github_dark';
+  String facebook = 'facebook_dark';
+  String instagram = 'instagram_dark';
+  String linkedin = 'linkedin_dark';
+  String twitter = 'twitter_dark';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +27,161 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Constants.navy,
         title: WebAppBar(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 32,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    onHover: (value) {
+                      if (value) {
+                        setState(() {
+                          github = 'github_light';
+                        });
+                      } else {
+                        setState(() {
+                          github = 'github_dark';
+                        });
+                      }
+                    },
+                    child: Container(
+                      child: Image.asset(
+                        'asset/$github.png',
+                        width: 20,
+                        height: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    onHover: (value) {
+                      if (value) {
+                        setState(() {
+                          linkedin = 'linkedin_light';
+                        });
+                      } else {
+                        setState(() {
+                          linkedin = 'linkedin_dark';
+                        });
+                      }
+                    },
+                    child: Image.asset(
+                      'asset/$linkedin.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    onHover: (value) {
+                      if (value) {
+                        setState(() {
+                          twitter = 'twitter_light';
+                        });
+                      } else {
+                        setState(() {
+                          twitter = 'twitter_dark';
+                        });
+                      }
+                    },
+                    child: Image.asset(
+                      'asset/$twitter.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    onHover: (value) {
+                      if (value) {
+                        setState(() {
+                          instagram = 'instagram_light';
+                        });
+                      } else {
+                        setState(() {
+                          instagram = 'instagram_dark';
+                        });
+                      }
+                    },
+                    child: Image.asset(
+                      'asset/$instagram.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    onHover: (value) {
+                      if (value) {
+                        setState(() {
+                          facebook = 'facebook_light';
+                        });
+                      } else {
+                        setState(() {
+                          facebook = 'facebook_dark';
+                        });
+                      }
+                    },
+                    child: Image.asset(
+                      'asset/$facebook.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Container(
+                      height: 200,
+                      width: 1,
+                      color: Constants.white,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+              Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
